729f31bd5de926d8e650200134b9be956c46fb3a
梁煜东 <375709770@qq.com>
2019-06-11 15:33:25 +0800

    完成Java高手之路示例代码

--- b2c-parent/b2c-core/src/main/java/org/javamaster/b2c/core/enums/BizExceptionEnum.java
+++ b2c-parent/b2c-core/src/main/java/org/javamaster/b2c/core/enums/BizExceptionEnum.java
@@ -8,7 +8,8 @@ public enum BizExceptionEnum {
     APPLICATION_ERROR(1000, "网络繁忙,请稍后再试"),
     INVALID_USER(1001, "用户名或密码错误"),
     INVALID_REQ_PARAM(1002, "参数错误"),
-    EXAM_NOT_FOUND(1003, "未查到考试信息");
+    EXAM_NOT_FOUND(1003, "未查到考试信息"),
+    ;
 
     BizExceptionEnum(Integer errorCode, String errorMsg) {
         this.errorCode = errorCode;
