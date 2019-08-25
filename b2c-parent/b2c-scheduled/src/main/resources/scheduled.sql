a3b8193c78d51aae8982acdad48d32f39b7bb413
jufeng98 <375709770@qq.com>
2019-08-25 11:14:18 +0800

    新增实现定时任务功能示例代码

--- b2c-parent/b2c-scheduled/src/main/resources/scheduled.sql
+++ b2c-parent/b2c-scheduled/src/main/resources/scheduled.sql
@@ -1,10 +1,15 @@
 drop table if exists `spring_scheduled_cron`;
 create table `spring_scheduled_cron` (
-  `cron_id`         int auto_increment primary key,
-  `cron_key`        varchar(128) not null unique,
-  `cron_expression` varchar(20)  not null,
-  `task_explain`    varchar(50),
-  `status`          tinyint comment '1:正常;2:停用'
+  `cron_id`         int primary key           auto_increment
+  comment '主键id',
+  `cron_key`        varchar(128) not null unique
+  comment '定时任务完整类名',
+  `cron_expression` varchar(20)  not null
+  comment 'cron表达式',
+  `task_explain`    varchar(50)  not null     default ''
+  comment '任务描述',
+  `status`          tinyint      not null     default 1
+  comment '状态,1:正常;2:停用'
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
